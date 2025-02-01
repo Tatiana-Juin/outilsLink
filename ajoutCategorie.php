d84f329b910aa29401fc56788bc6e18cd99b0d9d
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-02-01 12:20:20 +0100

    Ajout du bouton pour ajouter une categorie

--- ajoutCategorie.php
+++ ajoutCategorie.php
@@ -11,7 +11,8 @@
 
         <form action="" method="POST">
             <label for="nom_categorie">Nom categorie</label>
-            <input type="text" name="nom_categorie" id="nom_categorie" class="nom_categorie">   
+            <input type="text" name="nom_categorie" id="nom_categorie" class="nom_categorie">  
+            <input type="submit" value="btn_categorie" id="btn_categorie" class="btn_categorie"> 
         </form>
 
     </div>
