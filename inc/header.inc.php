79a9928389acb8ce54aac1ed6300cb09116c3bd7
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-02-01 12:23:02 +0100

    Ajout du fichier style.css dans le dossier assets / css et je l'ai relier dans le fichier header.inc.php

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?=RACINE_SITE."assets/css/nav.css" ?>">
+    <link rel="stylesheet" href="<?=RACINE_SITE."assets/css/style.css" ?>">
     
     <title><?= $titre ?></title>
 </head>
