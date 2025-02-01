589353199d6405734e39e70e68cdc757670318f8
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-02-01 12:19:24 +0100

    J'ai fait la structure HTML du formulaire d'ajout de categorie

--- ajoutCategorie.php
+++ ajoutCategorie.php
@@ -4,7 +4,20 @@
     require_once("inc/header.inc.php");
 ?>
 
-    
+ <main>
+    <h1>Ajout d'une categorie </h1>
+
+    <div class="div_categorie">
+
+        <form action="" method="POST">
+            <label for="nom_categorie">Nom categorie</label>
+            <input type="text" name="nom_categorie" id="nom_categorie" class="nom_categorie">   
+        </form>
+
+    </div>
+   
+
+ </main>
 
 <?php 
     require_once("inc/footer.inc.php");
