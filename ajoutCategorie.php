17afcd6307cec8240b6ea8f825c7360b9a0a4a67
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-02-07 18:03:08 +0100

    le style de ajout categorie a etait fait

--- ajoutCategorie.php
+++ ajoutCategorie.php
@@ -8,14 +8,15 @@
     <!-- TITRE DE LA PAGE  -->
     <h1 class="title-page">Ajout d'une categorie </h1>
 
-    <div class="divCategorie">
+    <div class="div-Categorie">
 
         <!-- AJOUT D'UN MESSAGE SI CELA EST UN -->
-        <p class="ajout-success"></p>
+        <p class="echec"></p>
 
-        <form action="" method="POST">
-            <label for="nom_categorie">Nom categorie</label>
-            <input type="text" name="nom_categorie" id="nom_categorie" class="nom_categorie">  
+        <form action="" method="POST" class="add_categorie">
+            <!-- NOM D'UNE CATEGORIE -->
+            <label for="nom_categorie" class="label_categorie">Nom categorie</label>
+            <input type="text" name="input_categorie" id="input_categorie" class="input_categorie" >  
             <input type="submit" value="Ajouter" id="btn_categorie" class="btn_categorie"> 
         </form>
 
