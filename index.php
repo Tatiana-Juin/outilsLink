a6da1258d71792d6e428020a08ca8a573ea94632
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-02-22 12:11:58 +0100

    J'ai fait le bouton de retour pour revenir sur tous les outils apres que l'on est fait la recherche

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
     require_once "inc/header.inc.php";
     // APPELLE DE LA FONCTION allOutil() POUR AFFICHER TOUS LES OUTILS 
     $allOutils = allOutil();
+    // INITIALISATION MOT EXISTE QUI PERMET D'AFFICHER LE BOUTON RETOUR
+    $motExiste = false;
 
+    $info ="";
     // POUR LA BARRE DE RECHERCHE 
     if(!empty($_POST)){
         $mot=trim($_POST['recherche']);
@@ -21,6 +24,12 @@
 
         if(!empty($recherche)){
             $allOutils = $recherche;
+        } 
+        // SI LE MOTS EXISTE PAS 
+        else{
+
+            $motExiste = false;
+            $info ="Le mot saisie est introuvable";
         }
 
     }
@@ -33,20 +42,26 @@
 
     <div class="card_container">
 
-            <?php
-                if(!empty($allOutils)){
-                    foreach ($allOutils as $key => $allOutil){ ?>
-                        <div class="card">
-                            <!-- POUR LE NOM DE L'OUTIL -->
+            <?php if(!empty($allOutils)){ 
+                // POUR AFFICHER LE BOUTON RETOUR QUI REVIENS EN ARRIERE SANS LA RECHERCHE 
+                if($motExiste ===true){ ?>
+                     <a href="<?=RACINE_SITE ?>index.php">retour</a>
+                <?php
+                }
+                // POUR AFFICHER LE MESSAGE ERREUR IL SUFFIT DE FAIRE UN ELSE ET DEMETTRE UN PARAGRAPHE EN APPELLANT $INFO PAR CONTRE IL FAUT REFAIRE LE STYLE CAR CELA S'AFFICHE A COTER ET NON EN DESSOUS JE PENSE QU'IL FAUT AJOUTER UNE DIV 
+                 foreach ($allOutils as $key => $allOutil){ ?>
+                    <div class="card">
+                        <!-- POUR LE NOM DE L'OUTIL -->
 
-                            <a href="<?=$allOutil['url_outil']?>" target="_blank" class="affiche_btn_outil">  
-                                <p class="affiche_nom_outil"><?= $allOutil['nom_outil'] ?> </p>
-                            </a>
+                        <a href="<?=$allOutil['url_outil']?>" target="_blank" class="affiche_btn_outil">  
+                            <p class="affiche_nom_outil"><?= $allOutil['nom_outil'] ?> </p>
+                        </a>
 
-                            <!-- NOM DE LA CATEGORIE  -->
-                            <p class="affiche_categorie"><?= $allOutil['nom_categorie'] ?></p>
+                        <!-- NOM DE LA CATEGORIE  -->
+                        <p class="affiche_categorie"><?= $allOutil['nom_categorie'] ?></p>
 
-                        </div>
+                        
+                    </div>
 
                 <?php 
                     } 
