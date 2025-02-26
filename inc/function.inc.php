a9469289e877f3741de7e650bf1f4d8df8f6a156
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-02-26 11:52:48 +0100

    Modification de la fonction categorieById car elle comporter une faute d'orthographe . J'ai egalement fait les verification pour affficher le nom d'une categorie

--- inc/function.inc.php
+++ inc/function.inc.php
@@ -75,7 +75,7 @@
         $sql = "SELECT nom_categorie FROM categorie WHERE id_categorie = :id_categorie";
         $requete = $pdo->prepare($sql);
         $requete->execute(array(
-            ":id_caegorie" =>$id_categorie
+            ":id_categorie" =>$id_categorie
         ));
         $resultat = $requete->fetch();
         return $resultat;
--- uneCategorie.php
+++ uneCategorie.php
@@ -2,7 +2,22 @@
     $titre = "Une categorie"; 
     require_once("inc/function.inc.php");
 
-    
+    // POUR LES ERREUR
+    $info ="";
+
+    if(isset($_GET['action']) && isset($_GET['idCategorie'])){
+
+        //on verifie que action n'est pas vide et que action et voir et que idCategorie n'est pas vide 
+      if(!empty($_GET['action']) && $_GET['action'] =="voir" && !empty($_GET['idCategorie'])){
+
+        // recupere de manière sécuriser id 
+        $idCategorie = htmlentities($_GET['idCategorie']);
+        $nomCategorie = categorieById($idCategorie);
+      }
+
+    }else{
+        header("location:categorie.php");
+    }
 
     // FIN PHP POUR EVITER LES BUG
     require_once("inc/header.inc.php");
@@ -10,6 +25,8 @@
 ?>
 
 <main>
+
+    
 
 </main>
 
