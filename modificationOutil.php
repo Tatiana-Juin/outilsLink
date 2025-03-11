7ae453c0c509ecaffba3f7e4c6091580d210b8fd
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-03-11 01:31:55 +0100

    Creation du fichier modificationOutil.php

---
+++ modificationOutil.php
@@ -0,0 +1,12 @@
+<?php
+    $titre = "Modification des informations de l'outil";
+    require_once("inc/function.inc.php");
+    require_once("inc/header.inc.php");
+?>
+<main>
+
+</main>
+
+<?php
+    require_once("inc/footer.inc.php");
+?>
