79ad059a758905687f39422fee09406a7315f913
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-03-21 11:33:12 +0100

    La fonctionnaté de suppression de l'outil fonctionne . Il me reste qu'a faire le design de cette page .

--- suppressionOutil.php
+++ suppressionOutil.php
@@ -3,25 +3,45 @@
     require_once("inc/function.inc.php");
     require_once("inc/header.inc.php");
 
-    if (isset($_GET['action']) && isset($_GET['idOutil'])) {
+    if (isset($_GET['action']) && isset($_GET['idOutil']) && is_numeric($_GET['idOutil'])) {
      
        if(!empty($_GET['action']) && $_GET['action'] == "delete" && !empty($_GET['idOutil'])){
-        // RECUPERE ID DE L'OUTIL
-        $idOutil = htmlentities($_GET['idOutil']);
 
-        // RECUPERE LES INFORMATIONS DE L'OUTIL
-        $informationOutil = outilById($idOutil);
+            // RECUPERE ID DE L'OUTIL
+            $idOutil = (int) htmlentities($_GET['idOutil']);
 
-        $idCategorie = $informationOutil['id_categorie'];
-        $informationCategorie = categorieById($idCategorie);
-        
+            // RECUPERE LES INFORMATIONS DE L'OUTIL
+            $informationOutil = outilById($idOutil);
+
+            // REDIRECTION SI ID N'EXISTE PAS 
+            if( (int) $idOutil != $informationOutil['id_outil'] ){
+                header("location:" . RACINE_SITE . "index.php");
+                exit();
+            }
+
+            // on verifie que c'est un tableau 
+            if(is_array($informationOutil)){
+                // recupère id 
+                $idCategorie = $informationOutil['id_categorie'];
+                $informationCategorie = categorieById($idCategorie);
+
+                // POUR LE BOUTON DE SUPPRESSION
+                if($_SERVER['REQUEST_METHOD'] =='POST' && isset($_POST['supprimer_outil'])){
+
+                    deleteOutilById($idOutil);
+                    header("Location: index.php");
+                    exit();
+                
+                }
+            }
+            else{
+                echo "Outil introuvable";
+            }
+      
        }
-        
-        
-
-    
-       
     } 
+
+  
     
 ?>
 
@@ -30,16 +50,19 @@
     <!-- titre de la page  -->
     <h1 class="title-page">Suppression de l'outils </h1>
 
-   
-    <!-- info relative a l'outil  -->
-    <p class="info-outil"> Nom : <?=$informationOutil['nom_outil']?></p>
-    <p class="info-outil"> Categorie : <?=$informationCategorie['nom_categorie']?></p>
+   <?php if(isset($informationOutil) && is_array($informationOutil)){ ?>
 
-    <!-- POUR LA SUPPRESION - BOUTON ETC  -->
-    <form action="" method="POST">
-        <input type="hidden" name="<?=$informationOutil['id_outil'] ?>" value="<?= $informationOutil['id_outil']?>">
-        <input type="submit" value="Supprimer" name="supprimer_outil" class="btn-outil">
-    </form>
+    <!-- info relative a l'outil  -->
+        <p class="info-outil"> Nom : <?=$informationOutil['nom_outil']?></p>
+        <p class="info-outil"> Categorie  : <?=$informationCategorie['nom_categorie']?></p>
+    
+        <!-- POUR LA SUPPRESION - BOUTON ETC  -->
+        <form action="" method="POST">
+            <input type="hidden" name="<?=$informationOutil['id_outil'] ?>" value="<?= $informationOutil['id_outil']?>">
+            <input type="submit" value="Supprimer" name="supprimer_outil" class="btn-outil">
+        </form>
+
+    <?php } ?>
     
 </main>
 
